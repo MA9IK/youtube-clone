f62c1c6a3586dfae29d2945867dd24ff8c2227c9
ivan <developerjsua@gmail.com>
2023-07-31 21:25:37 +0300

    fix feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
-import Sidebar from './Sidebar';
+import { Sidebar, Videos } from './';
 
 const Feed = () => {
   return (
@@ -13,8 +13,19 @@ const Feed = () => {
         }}>
         <Sidebar />
         <Typography className='copyright' sx={{ mt: 1.5, color: '#fff' }}>
-          copyright JSM mastery
+          Copyright 2023 JSM Mastery
         </Typography>
+      </Box>
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+        <Typography
+          variant='h4'
+          fontWeight='bold'
+          mb={2}
+          sx={{ color: 'white' }}>
+          New <span style={{ color: '#F31503' }}>Videos</span>
+        </Typography>
+
+        <Videos videos={[]} />
       </Box>
     </Stack>
   );
